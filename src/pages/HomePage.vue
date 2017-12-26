763fdb862d49ea7224d64f91ed90cd270f0b8a64
larisa shousterman <lshousterman@gmail.com>
2017-12-26 14:48:25 +0200

    list cpm

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,46 +1,77 @@
 <template>
   <section>
        <Nav></Nav>
-    <h1>TEAMUP</h1>
-    <Detailes :teamOb="teamOb"></Detailes>
+    <!-- TODO:erasethis -->
+    <button>test btn bring category</button> 
+    <!-- TODO: end of erase -->
+    <category-list v-if="games" :games="games"></category-list>
+    <category-detailes :teamOb="teamOb"></category-detailes>
+    
  
   </section>
 </template>
 
 <script>
-import Detailes from '../team/cmps/Detailes'
-import Nav from '../components/Nav'
+import categoryDetailes from "../team/cmps/categoryDetailes";
+import categoryList from "../team/cmps/categoryList";
+import Nav from "../components/Nav";
 
 export default {
-  name: 'HomePage',
+  name: "HomePage",
   data() {
     return {
-      teamOb:{
+      teamOb: {
         category: "volybool",
         logo: "logo",
         name: "fun-voly",
         type: "public",
         membersCount: 10,
         membersLimit: 12,
-        Location:{
-          adress:"shenkin 42, tel aviv"
+        Location: {
+          adress: "shenkin 42, tel aviv"
         },
         schedule: "25/12/17"
-      }
+      },
+      games: [
+        {
+          id: 1,
+          membersCount: 8,
+          membersLimit: 12,
+          Location: {
+            adress: "shenkin 42, tel aviv"
+          }
+        },
+        {
+          id: 2,
+          membersCount: 10,
+          membersLimit: 12,
+          Location: {
+            adress: "rohama 18, tel aviv"
+          }
+        },
+        {
+          id: 3,
+          membersCount: 12,
+          membersLimit: 12,
+          Location: {
+            adress: "habonim 04, tel aviv"
+          }
+        }
+      ]
     };
   },
-  components:{
-    Detailes,
-    Nav
+  components: {
+    categoryDetailes,
+    Nav,
+    categoryList
   }
 };
 </script>
 
 <style scoped>
-
-h1{
-    font-family: var(--primary-font);
-    color: var(--font-main-color);  
+h1 {
+  font-family: var(--primary-font);
+  color: var(--font-main-color);
 }
 </style>
 
---
+++ src/team/cmps/categoryList.vue
@@ -0,0 +1,68 @@
+<template>
+   <section class="team-list">
+       <ul>
+            <li  v-for="game in games" :key="game.id">
+                <ul class="game-item">
+                    <li>
+                        {{game.membersCount}} / {{game.membersLimit}} 
+                    </li>
+         
+                    <li>
+                        {{game.Location.adress}}
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </section>
+
+</template>
+
+<script>
+export default {
+  props: ["games"],
+
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style scoped>
+.team-list {
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  left: 500px;
+  align-items: center;
+  height: 90vh;
+  width: 90vh;
+}
+h2 {
+  font-family: Helvetica;
+  color: var(--font-main-color);
+  height: 2em;
+  text-shadow: 1px 1px 2px black;
+}
+h1 {
+  font-family: Helvetica;
+  font-size: 2em;
+  color: var(--font-main-color);
+  height: 3em;
+  text-shadow: 2px 2px 4px peru;
+}
+.game-item {
+  background-color: peru;
+  border-style: solid;
+  border-width: 1px;
+}
+ul{
+    list-style-type: none;
+}
+</style>
+
+
+
+
+
+
+
