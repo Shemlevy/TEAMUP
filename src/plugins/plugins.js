9653f4c00f3f241c32396b24c52027d2eea135a0
larisa shousterman <lshousterman@gmail.com>
2017-12-30 18:41:37 +0200

    detailespage

--- src/plugins/plugins.js
+++ src/plugins/plugins.js
@@ -3,10 +3,14 @@ import Vuetify from 'vuetify'
 import VueRouter from 'vue-router'
 import Vuex from 'vuex'
 import Vuelidate from 'vuelidate'
+import Carousel3d from 'vue-carousel-3d';
+
+
 
 Vue.use(Vuex)
 Vue.use(Vuetify)
 Vue.use(VueRouter)
+Vue.use(Carousel3d);
 Vue.use(Vuetify, {
     theme: {
       primary: '#3767c8',
@@ -14,3 +18,5 @@ Vue.use(Vuetify, {
       
     }
   })
+
+  
