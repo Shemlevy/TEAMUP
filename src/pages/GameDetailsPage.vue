16c96d7fb3ebc9bb352aba0f8b771da4f8a19e21
larisa shousterman <lshousterman@gmail.com>
2018-01-03 21:15:06 +0200

    game detailes changed

--- src/pages/GameDetailsPage.vue
+++ src/pages/GameDetailsPage.vue
@@ -22,6 +22,20 @@
           </v-list-tile-content>
         </v-list-tile>
         <v-divider inset></v-divider>
+         <v-list-tile @click="">
+            <v-list-tile-action>
+              <v-icon color="indigo">people</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-sub-title>PLAYERS COUNT</v-list-tile-sub-title>
+              <v-list-tile-title>{{game.players.length}}/{{game.playerLimit}} </v-list-tile-title>
+              <v-list-tile-sub-title v-if="playersLeft === 0" style="color: red">NO PLACES LEFT</v-list-tile-sub-title>
+              <v-list-tile-sub-title v-if="playersLeft === 1" style="color: red">{{playersLeft}} PLACE LEFT</v-list-tile-sub-title>
+              <v-list-tile-sub-title v-if="playersLeft > 1" style="color: red">{{playersLeft}} PLACES LEFT</v-list-tile-sub-title>
+              
+            </v-list-tile-content>
+        </v-list-tile>
+          <v-divider inset></v-divider>
         <v-list-tile @click="">
             <v-list-tile-action>
               <v-icon color="indigo">grade</v-icon>
@@ -55,17 +69,13 @@
               </v-list-tile-content>
             </v-list-tile>
             <v-divider inset></v-divider>
-            <v-list-tile @click="">
-              <v-list-tile-action>
-                <v-icon color="indigo">chat</v-icon>
-              </v-list-tile-action>
-              <v-list-tile-content>
-                <v-list-tile-sub-title>CHAT</v-list-tile-sub-title> 
-              </v-list-tile-content>
-            </v-list-tile>
              <v-list-tile-sub-title>
-                <button class="main-btn" v-if="user && !exist" @click="userJoinGame">TEAM<span>UP</span></button>
-                <button class="main-btn" v-if="user && exist" @click="leaveGame">Leave</span></button>
+                <button class="main-btn" v-if="user && !exist && canJoinGame" @click="userJoinGame">TEAM<span>UP</span></button>
+                <button class="main-btn" v-if="user && exist" @click="leaveGame">Leave Game</span></button>
+                <div v-if="exist">
+                  <div style="font-size:16px; color:blue; padding-left: 35px;">YOU ARE A PLAYER IN THIS GAME</div>
+                </div>
+                
             </v-list-tile-sub-title>
           </v-list>
           <GameMembers :game="game"></GameMembers>          
@@ -91,11 +101,14 @@ export default {
   data() {
    
     return {
-      exist : false
+      exist : false,
+      canJoinGame: true,
+      playersLeft: -100
     };
   },
   computed: {
     game() {
+       
       var strGame = JSON.stringify(this.$store.getters[GET_SELCTED_GAME]);
       return JSON.parse(strGame);
     },
@@ -106,30 +119,41 @@ export default {
       return this.game.category.url;
     }
   },
-  created (){
+  created(){
       console.log('page created')
       var id = this.$router.history.current.params.gameId
       this.$store.dispatch({type: LOAD_GAME_BY_ID , gameId: id})
+      checkLimit();
+      
       
       
   },
   watch:{
     game() {
        this.setActions();
+       this.checkLimit();
     },
     user() {
        this.setActions();
+       this.checkLimit();
     }
   },
   methods: {
     setActions() {
       if (this.game && this.user) {
-        this.exist = this.game.players.find(player => player.id === this.user._id) 
-      }
-    },
+        this.exist = false;
+        this.exist = this.game.players.find(player => player.id === this.user._id)
+      }},
+    checkLimit(){
+      if(this.game){
+        this.playersLeft = this.game.playerLimit - this.game.players.length;
+        if(this.game.players.length >= this.game.playerLimit){
+          this.canJoinGame = false;
+        }else{
+          this.canJoinGame = true;
+        }
+      }},
      userJoinGame(){
-      
-
       this.game.players.push({
         id: this.user._id,
         name: this.user.name,
@@ -167,4 +191,9 @@ export default {
 .game-detailes-cover{
   position: relative;
 }
+.main-btn{
+  background-color: #e0dada;
+  margin: 15px;
+}
+
 </style>
